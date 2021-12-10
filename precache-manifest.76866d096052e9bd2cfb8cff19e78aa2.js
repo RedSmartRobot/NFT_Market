5e12f28515d3a6ec489cdec2b8d17854286e050d
RedSmartRobot <sergey.budylin@gmail.com>
2021-12-10 19:30:18 +0300

    Updates

---
+++ precache-manifest.76866d096052e9bd2cfb8cff19e78aa2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8203e1c7f71eb68948ec",
+    "url": "/NFT_Market/static/css/main.4415e2f9.chunk.css"
+  },
+  {
+    "revision": "8203e1c7f71eb68948ec",
+    "url": "/NFT_Market/static/js/main.8203e1c7.chunk.js"
+  },
+  {
+    "revision": "ceb160c4e8c5e4ca8832",
+    "url": "/NFT_Market/static/js/1.ceb160c4.chunk.js"
+  },
+  {
+    "revision": "85b900b77415ed089fdc",
+    "url": "/NFT_Market/static/js/runtime~main.85b900b7.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/NFT_Market/static/media/loading.26ef8bb4.gif"
+  },
+  {
+    "revision": "fbf33967fa244d21d61fb85f233fc331",
+    "url": "/NFT_Market/static/media/metamask.fbf33967.svg"
+  },
+  {
+    "revision": "ade89e2820be3a2eaef03f6e477d1631",
+    "url": "/NFT_Market/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/NFT_Market/precache-manifest.76866d096052e9bd2cfb8cff19e78aa2.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/NFT_Market/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
